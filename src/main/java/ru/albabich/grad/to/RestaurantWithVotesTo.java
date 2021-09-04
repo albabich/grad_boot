0cf86c83f65bb79bdb4dd4ecede4cb2bee0a10b7
Alexander Babich <albabich@gmail.com>
2021-09-04 21:37:50 +0300

    Fix

--- src/main/java/ru/albabich/grad/to/RestaurantWithVotesTo.java
+++ src/main/java/ru/albabich/grad/to/RestaurantWithVotesTo.java
@@ -1,30 +1,16 @@
 package ru.albabich.grad.to;
 
-import lombok.ToString;
+import lombok.*;
 
-import java.beans.ConstructorProperties;
-import java.util.Objects;
-
+@EqualsAndHashCode(callSuper = true)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(callSuper = true)
 public class RestaurantWithVotesTo extends NamedTo {
-    private final int voteCounter;
+    private int voteCounter;
 
-    @ConstructorProperties({"id", "name", "voteCounter"})
     public RestaurantWithVotesTo(int id, String name, int voteCounter) {
         super(id, name);
         this.voteCounter = voteCounter;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RestaurantWithVotesTo that = (RestaurantWithVotesTo) o;
-        return voteCounter == that.voteCounter && name.equals(that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, voteCounter);
     }
 }
