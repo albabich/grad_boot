651970a48dc55a14428a72fec1eb5d9cc09745d2
Alexander Babich <albabich@gmail.com>
2021-09-06 12:41:23 +0300

    Check NPE

--- src/main/java/ru/albabich/grad/util/RestaurantUtil.java
+++ src/main/java/ru/albabich/grad/util/RestaurantUtil.java
@@ -11,7 +11,7 @@ public class RestaurantUtil {
     public static List<RestaurantWithVotesTo> getTosWithVotes(Collection<Restaurant> restaurants) {
         return restaurants.stream()
                 .map(restaurant -> new RestaurantWithVotesTo(restaurant.getId(),
-                        restaurant.getName(), restaurant.getVotes().size()))
+                        restaurant.getName(), restaurant.getVotes() != null ? restaurant.getVotes().size() : 0))
                 .toList();
     }
 
